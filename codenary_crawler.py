906885075f7787563b34bb1cc9daf7f85bcce89a
jujaemin <147119777+jujaemin@users.noreply.github.com>
2023-11-06 21:50:24 +0900

    codenary_crawler.py

--- codenary_crawler.py
+++ codenary_crawler.py
@@ -30,21 +30,17 @@ class codenary_crawl:
                 
                 self.append([company_name, company_location, stack.text])
             
-    def fetch_tech_stack(self, url):
-
-        res = requests.get(url)    
-        soup = BeautifulSoup(res.text, "html.parser")
+    def get_tech_info(self, url):
+        stack_res = requests.get(url)
+        stack_soup = BeautifulSoup(stack_res.text, 'html')
         
-        # a 태그 중 필요한 범위 내에서 스택명, 스택 분류 스크래핑하여 리스트에 저장하기
-        for a_tag in soup.find_all("a")[7:-2]:
-            tech_stack = a_tag.get('id')
-            
-            # 스택명 별로 상세 페이지에서 스택 분류 스크래핑하기
-            detail_page = f"https://www.codenary.co.kr/techstack/detail/{tech_stack}"
-            detail_res = requests.get(detail_page)
-            detail_soup = BeautifulSoup(detail_res.text, "html.parser")
-
-            tech_type = detail_soup.find("div", class_="mantine-Text-root mantine-7keej3").text
-            
-            # 리스트에 저장하기
-            self.append([tech_type, tech_stack])
+        parents = stack_soup.find('div','mantine-Grid-root mantine-5af20u')
+        dev_stacks = parents.find_all('div', 'mantine-Grid-col mantine-ne8237')
+        
+        for dev_stack in dev_stacks:
+        
+            dev = dev_stack.find('div', 'mantine-j3bl35')
+            stack_name  = dev.find('div', 'mantine-Text-root mantine-xn0h5k').text
+            stack_class  = dev.find('div', 'mantine-Text-root mantine-8ytu7x').text
+        
+            self.append([stack_name, stack_class])
