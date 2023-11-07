18d8c5a7d6a3359867d0bd004f54b26de1a8f412
woo-lala <hyunj2034@naver.com>
2023-11-07 13:37:21 +0900

    feat: delete address field, add sido, sigg field in Company model and change type field in TechStack model

--- proj01/main/models.py
+++ proj01/main/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # 기업 정보
 class Company(models.Model):
     name = models.CharField(max_length=50, unique=True)            # 기업명
-    address = models.CharField(max_length=100, default='없음')     # 기업 위치(주소)
+    sido = models.CharField(max_length=50, default='시도') # 기업 위치 (시도)
+    sigg = models.CharField(max_length=50, default='시군구')     # 기업 위치(시군구)
     job_positions = models.ManyToManyField('JobPosition', related_name='companies') 
     # 직무 정보와 다대다 관계 정의
     
@@ -24,13 +25,8 @@ class JobPosition(models.Model):
 
 # 기술스택 정보
 class TechStack(models.Model):
-    CATEGORY_CHOICES = ( # 카테고리 추가 가능 -> software | tool..등
-        ('language', 'Language'),
-        ('framework', 'Framework'),
-        ('tool', 'Tool')
-    )
-    name = models.CharField(max_length=50, unique=True) 
-    type = models.CharField(max_length=20, choices=CATEGORY_CHOICES) # 보류
+    name = models.CharField(max_length=50, unique=True)
+    type = models.CharField(max_length=50, blank=True) # 보류
     icon_url = models.URLField(blank=True)  # 선택사항
 
     def __str__(self):
