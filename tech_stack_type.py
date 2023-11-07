2f073800fd5b47f8270d11f9063264944b22083e
deun115 <141890722+deun115@users.noreply.github.com>
2023-11-07 23:59:00 +0900

    Add files via upload
    Initial Commit: Adding Stack-Type to Crawled-Data

---
+++ tech_stack_type.py
@@ -0,0 +1,39 @@
+tech_stack = {
+    # 프로그래밍 언어
+    'programming_languages': [
+        'php', 'lua', 'objective-c', 'python', 'c', 'c++', 'c#', 'java', 'r', 'go',
+        'swift', 'kotlin', 'typescript', 'javascript', 'sql', 'vb.net', 'scala',
+        'haskell', 'dart', 'rust', 'f#', 'perl', 'ruby', 'bash'
+    ],
+    # 프레임워크
+    'frameworks': [
+        'django', 'flask', 'bootstrap', 'react.js', 'angular', 'vue.js', 'nestjs', 'spring',
+        'fastapi', 'node.js', 'expressjs', 'xcode', 'laravel', 'tensorflow', 'keras',
+        'pytorch', 'unity', 'electron', 'qt', 'asp.net', 'mvc', 'redux', 'angularjs',
+        'tensorflow', 'fastlane', 'webpack', 'nest.js', 'backendless'
+    ],
+    # 라이브러리
+    'libraries': [
+        'opencv', 'ffmpeg', 'react', 'redux-saga', 'jquery', 'grpc', 'openssl', 'scikit-learn',
+        'pandas', 'numpy', 'matplotlib', 'jsonapi', 'typeorm', 'gunicorn', 'rxjs', 'rxswift'
+    ],
+    # 데이터베이스
+    'databases': [
+        'mysql', 'mssql', 'postgresql', 'redis', 'mongodb', 'oracle', 'sqlite', 'dynamodb',
+        'mariadb', 'neo4j'
+    ],
+    # 데브옵스
+    'devops_tools': [
+        'docker', 'kubernetes', 'ansible', 'terraform', 'jenkins', 'aws', 'azure', 'gcp',
+        'nginx', 'git', 'github', 'gitlab', 'bitbucket', 'grafana', 'splunk', 'sentry',
+        'elk', 'prometheus', 'argocd', 'argo', 'sourcetree', 'codecommit', 'confluence'
+    ],
+    # 툴
+    'tools': [
+        'selenium', 'autocad', 'slack', 'postman', 'cypress', 'jira', 'notion', 'fiddler',
+        'redash', 'zeplin', 'figma', 'git', 'webpack', 'babel', 'resharper', 'teamcity',
+        'visual', 'studio', 'eclipse', 'matlab', 'labview', 'solidworks', 'xenserver',
+        'vmware', 'office', 'excel', 'notion.so', 'subversion', 'vercel', 'sourcetree',
+        'sentry', 'fastlane', 'resharper'
+    ]
+}
