372398cb32512b2ba81cb2940a2f7832926ea7c0
Jung Hun Soo <hunsoodev@gmail.com>
2023-11-11 20:53:20 +0900

    Final correction

--- proj01/main/views.py
+++ proj01/main/views.py
@@ -1,7 +1,7 @@
 import json
 import plotly.express as px
 
-from django.http import JsonResponse, HttpResponseRedirect
+from django.http import JsonResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
@@ -66,139 +66,70 @@ def result_view(request) :
         ).count()
 
     # 선택한 직무에 따른 결과 처리 : 직무별 시각화 그래프
-    job_position = get_object_or_404(JobPosition, name=request.POST['job_position'])
-    tech_stacks = TechStack.objects.filter(job_positions=job_position)
+    try:
+        job_position = get_object_or_404(JobPosition, name=request.POST['job_position'])
+    except Http404:
+        messages.error(request, '다시 선택해주세요.')
+        return HttpResponseRedirect(reverse('main:main'))
+    else:
+        tech_stacks = TechStack.objects.filter(job_positions=job_position)
 
-    tech_type_counts = tech_stacks.values('type').annotate(count=Count('name')).order_by('-count')[:5]
+        tech_type_counts = tech_stacks.values('type').annotate(count=Count('name')).order_by('-count')[:5]
 
-    charts_html = []  # 차트 HTML을 저장할 리스트
+        charts_html = []  # 차트 HTML을 저장할 리스트
 
-    for tech_stack in tech_type_counts.values('type'):
+        for tech_stack in tech_type_counts.values('type'):
 
-        cls = tech_stack['type']
-        tech_stacks_by_cls = tech_stacks.filter(type=cls)
-        # -----
-        data = [{'스택': ts.name, '개수': tech_stacks.filter(name=ts.name).count()} for ts in tech_stacks_by_cls]
+            cls = tech_stack['type']
+            tech_stacks_by_cls = tech_stacks.filter(type=cls)
+            # -----
+            data = [{'스택': ts.name, '개수': tech_stacks.filter(name=ts.name).count()} for ts in tech_stacks_by_cls]
 
-        unique_data = {}
-        for item in data:
-            stack_name = item['스택']
-            count = item['개수']
-            if stack_name not in unique_data:
-                unique_data[stack_name] = count
+            unique_data = {}
+            for item in data:
+                stack_name = item['스택']
+                count = item['개수']
+                if stack_name not in unique_data:
+                    unique_data[stack_name] = count
 
-        unique_data_list = [{'스택': key, '개수': value} for key, value in unique_data.items()]
+            unique_data_list = [{'스택': key, '개수': value} for key, value in unique_data.items()]
 
-        # '개수' 필드에 따라 내림차순으로 정렬
-        sorted_data = sorted(unique_data_list, key=lambda x: x['개수'], reverse=True)
+            # '개수' 필드에 따라 내림차순으로 정렬
+            sorted_data = sorted(unique_data_list, key=lambda x: x['개수'], reverse=True)
 
-        # 상위 5개 요소 선택
-        top_5_data = sorted_data[:5]
+            # 상위 5개 요소 선택
+            top_5_data = sorted_data[:5]
 
-        # Plotly 차트 생성
-        if len(sorted_data) < 5 : 
-            fig = px.pie(data_frame=sorted_data, values='개수', names='스택', title=f"{cls}")
-        else :
-            fig = px.pie(data_frame=top_5_data, values='개수', names='스택', title=f"{cls} TOP5")
-        fig.update_traces(hole=.3)
-        fig.update_traces(textposition='inside', textinfo='label+percent', textfont_size=14, textfont_color="white")
+            # Plotly 차트 생성
+            if len(sorted_data) < 5 :
+                fig = px.pie(data_frame=sorted_data, values='개수', names='스택', title=f"{cls}")
+            else :
+                fig = px.pie(data_frame=top_5_data, values='개수', names='스택', title=f"{cls} TOP5")
+            fig.update_traces(hole=.3)
+            fig.update_traces(textposition='inside', textinfo='label+percent', textfont_size=14, textfont_color="white")
 
-        fig.update_layout(
-            title_x=0.5,  # 제목 가운데 정렬
-            showlegend=True,  # 범례 표시 여부
-            legend=dict(
-                orientation="h",  # 범례 가로 방향
-                yanchor="bottom",  # 범례 기준점
-                y=-0.3,  # 범례 위치
-                xanchor="center",  # 범례 중앙 정렬
-                x=0.5,  # 범례 중앙 정렬
-            ),
-        )
+            fig.update_layout(
+                title_x=0.5,  # 제목 가운데 정렬
+                showlegend=True,  # 범례 표시 여부
+                legend=dict(
+                    orientation="h",  # 범례 가로 방향
+                    yanchor="bottom",  # 범례 기준점
+                    y=-0.3,  # 범례 위치
+                    xanchor="center",  # 범례 중앙 정렬
+                    x=0.5,  # 범례 중앙 정렬
+                ),
+            )
 
-        # 차트를 HTML 문자열로 변환하고 리스트에 추가
-        charts_html.append(fig.to_html(full_html=False))
+            # 차트를 HTML 문자열로 변환하고 리스트에 추가
+            charts_html.append(fig.to_html(full_html=False))
 
-    context = {
-        'charts_html': charts_html,
-        'selected_job_position': selected_job_position,
-        'selected_sido': selected_sido,
-        'selected_sigg': selected_sigg,
-        'selected_job_description': JobPosition.objects.filter(name=selected_job_position).values('description')[0]['description'],
-        'job_positing_counts' : job_positing_counts
-    }
-
-    return render(request, 'main/result.html', context)
-
-
-
-def temp_index(request):
-    #데이터베이스에서 직무 가져오기 -> flat=True를 이용해 리스트로 가져옴
-    job_position_list = JobPosition.objects.values_list('name', flat=True).distinct()
-
-    #데이터베이스에서 시도 가져오기 -> flat=True를 이용해 리스트로 가져옴
-    sido_list = Company.objects.values_list('sido', flat=True).distinct()
-
-    #시도에 해당하는 시군구 가져오기
-    sigg_dict = {}
-    for sido in sido_list:
-        sigg_list = Company.objects.filter(sido=sido).values_list('sigg', flat=True).distinct()
-        sigg_dict[sido] = list(sigg_list)
-
-    #자바스크립트에서 딕셔너리 제대로 가져다 쓰기 위해 json 형식으로
-    sigg_dict_json = json.dumps(sigg_dict)
-
-    return render(request, 'main/temp_index.html', {'job_position_list' : job_position_list, 'sido_list':sido_list, 'sigg_dict_json':sigg_dict_json})
-
-
-
-# 차트 그리기
-def temp_result(request):
-    #선택된 값 result 페이지에 넘겨서 확인하기
-    job_position = get_object_or_404(JobPosition, name=request.POST['job_position'])
-    tech_stacks = TechStack.objects.filter(job_positions=job_position)
-
-    sido = request.POST['sido']
-    sigg = request.POST['sigg']
-
-    tech_type_counts=tech_stacks.values('type').annotate(count=Count('name')).order_by('-count')[:5]    
-
-    charts_html = []  # 차트 HTML을 저장할 리스트
-
-    for tech_stack in tech_type_counts.values('type'):
-
-        cls = tech_stack['type']
-        tech_stacks_by_cls = tech_stacks.filter(type=cls)
-        #-----
-        data = [{'스택': ts.name, '개수': tech_stacks.filter(name=ts.name).count()} for ts in tech_stacks_by_cls]
-
-        unique_data = {}
-        for item in data:
-            stack_name = item['스택']
-            count = item['개수']
-            if stack_name not in unique_data:
-                unique_data[stack_name] = count
-
-        unique_data_list = [{'스택': key, '개수': value} for key, value in unique_data.items()]
-
-        # '개수' 필드에 따라 내림차순으로 정렬
-        sorted_data = sorted(unique_data_list, key=lambda x: x['개수'], reverse=True)
-
-        # 상위 5개 요소 선택
-        top_5_data = sorted_data[:5]
-
-        # Plotly 차트 생성
-        fig = px.pie(data_frame=top_5_data, values='개수', names='스택', title=f"{job_position.name} 직무의 {cls} 분류 TOP5")
-        fig.update_traces(hole=.3)
-        fig.update_traces(textposition='inside', textinfo='label+percent+value', textfont_size=17, textfont_color="white")
-
-        # 차트를 HTML 문자열로 변환하고 리스트에 추가
-        charts_html.append(fig.to_html(full_html=False))
-
-    context = {
-        'charts_html': charts_html, 
-        'job_position': job_position, 
-        'sido': sido,
-        'sigg':sigg
+        context = {
+            'charts_html': charts_html,
+            'selected_job_position': selected_job_position,
+            'selected_sido': selected_sido,
+            'selected_sigg': selected_sigg,
+            'selected_job_description': JobPosition.objects.filter(name=selected_job_position).values('description')[0]['description'],
+            'job_positing_counts' : job_positing_counts
         }
 
-    return render(request, 'main/temp_result.html', context)
+        return render(request, 'main/result.html', context)
