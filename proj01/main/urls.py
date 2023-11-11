323aa1bbc743db925511799087f712bc8ca93731
Jung Hun Soo <hunsoodev@gmail.com>
2023-11-11 18:48:45 +0900

    delete temp files

--- proj01/main/urls.py
+++ proj01/main/urls.py
@@ -8,8 +8,4 @@ urlpatterns = [
     path('', index, name='main'),
     path('result/', views.result_view, name='result_view'),
     path('get_sigg_list/', get_sigg_list, name='get_sigg_list'),
-
-    path('temp_index/', temp_index, name='temp_index'),
-    path('temp_result/', temp_result, name='temp_result')
-
 ]
