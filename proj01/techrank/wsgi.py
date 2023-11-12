d5960880d423717c30f63fae43e38b3355bbac51
Jung Hun Soo <hunsoodev@gmail.com>
2023-11-12 15:16:25 +0900

    files name/code refactoring

--- proj01/techrank/wsgi.py
+++ proj01/techrank/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj01.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techrank.settings')
 
 application = get_wsgi_application()
